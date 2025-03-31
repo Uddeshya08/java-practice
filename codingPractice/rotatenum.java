bd50a67868d20efae220911ffb09ffc9db93167e
Uddeshya Sharma <47824972+Uddeshya08@users.noreply.github.com>
2025-03-31 23:44:18 +0530

    Update rotatenum.java

--- codingPractice/rotatenum.java
+++ codingPractice/rotatenum.java
@@ -6,29 +6,40 @@ around which we need to right rotate the array. How to quickly print the right r
 Input: Array[] = {1, 3, 5, 7, 9}, K = 2.
 Output: 7 9 1 3 5
 
+/* k=3;
+size=5;
+roarr[1]=nums[3]
+roarr[2]=nums[4]
+roarr[3]=nums[5]
+
+roarr[4]=nums[0]
+roarr[5]nums[1]
+    */
+
  **/
-public class rotatenum {
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+         
+    int size = nums.length;
+        k = k % size; // Handle cases where k > size
 
-    public static void main(String[] args) {
+        int[] roarr = new int[size]; // New array for rotated values
 
-        int[] rotate={1,3,5,7,9};
-        int size=rotate.length;
-        int k=3;
-        int [] roarr=new int[5];
-
-
-            for (int i = k; i <= size - 1; i++) {
-                int temp = rotate[i];
-                roarr[i] = temp;
-                System.out.print(roarr[i]+", ");
-
-            }
-            for (int j = 0; j <= k - 1; j++) {
-                int temp = rotate[j];
-                rotate[j] = temp;
-               System.out.print(rotate[j]+", ");
-            }
-
+        // Copy last k elements to the beginning of new array
+        for (int i = 0; i < k; i++) {
+            roarr[i] = nums[size - k + i];
         }
 
+        // Copy the first (size-k) elements after the last k elements
+        for (int i = 0; i < size - k; i++) {
+            roarr[k + i] = nums[i];
+        }
+
+        // Copy back to original array
+        System.arraycopy(roarr, 0, nums, 0, size);
+
+
+        
     }
+}
